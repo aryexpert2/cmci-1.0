919b642906acf967406f54a8cedde19a2e0c28f3
aryexpert2 <cedouariel@gmail.com>
2024-02-10 11:13:36 -0500

    add functionnality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,53 @@
-import { Label, TextInput, Button } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() }); 
+    // la methode trim() permet de supprimer les espaces inutiles au début et à la 
+    // fin de la chaine de caractères lors de la saisie dans le formulaire  
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // evite un rafraichissement de la page qui fera perdre les données du formulaire 
+
+    if(!formData.username || !formData.email || !formData.password) {
+      return  setErrorMessage("Please fill in all the fields!");
+    }
+
+    try {
+      setLoading(true); // on désactive le bouton pendant le chargement
+      setErrorMessage(null); // on efface le message d'erreur
+      // res est la promesse retournée par fetch pour savoir si la requête va bien se passer ou non
+      // on va se servir d'un proxy pour éviter de mettre des URL complètes
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData), // on transforme l'objet JSON(formData) en une chaine JSON
+      });
+      const data = await res.json(); // on récupère les données de la promesse res sous forme de JSON
+      if(data.success === false) {
+        setErrorMessage(data.message); // erreur liée au user
+      }
+      setLoading(false); // on réactive le bouton après le chargement
+      if(res.ok) {
+        navigate("/sign-in"); // on redirige l'utilisateur vers la page de connexion
+      }
+    } catch (error) {
+      setErrorMessage(error.message); // erreur liée au serveur
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -24,21 +69,29 @@ export default function SignUp() {
         </div>
 
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div className="">
               <Label value="your username" />
-              <TextInput type="text" placeholder="John Doe" id="username" />
+              <TextInput type="text" placeholder="John Doe" id="username" onChange={handleChange} />
             </div>
             <div className="">
               <Label value="your email" />
-              <TextInput type="text" placeholder="name@company.com" id="email" />
+              <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange} />
             </div>
             <div className="">
               <Label value="your password" />
-              <TextInput type="password" placeholder="123456" id="password" />
+              <TextInput type="password" placeholder="123456" id="password" onChange={handleChange} />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size="sm" />
+                    <span className="pl-3">Loading...</span>
+                  </>
+                )
+                 : "Sign Up"
+              }
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -47,6 +100,13 @@ export default function SignUp() {
                 Sign in
               </Link>
         </div>
+        {
+          errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )
+        }
       </div>
     </div>
   </div>
