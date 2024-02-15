75bf352a13b18e1d43acf7a61fd91cade27a1673
aryexpert2 <cedouariel@gmail.com>
2024-02-14 19:26:07 -0500

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -12,7 +12,8 @@ import {
   updateFailure,
   deleteUserStart,
   deleteUserSuccess,
-  deleteUserFailure
+  deleteUserFailure,
+  signOutSuccess
  } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -143,6 +144,22 @@ export default function DashProfile() {
     }
   }
 
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST"
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signOutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center fond-semibold text-3xl'>Profile</h1>
@@ -198,7 +215,7 @@ export default function DashProfile() {
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={()=> setShowModal(true)} className="cursor-pointer">Delete Account</span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="cursor-pointer">Sign Out</span>
       </div>
       {
         updateUserSuccess && (<Alert className='mt-5' color="success">{updateUserSuccess}</Alert>)
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,34 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { signOutSuccess } from "../redux/user/userSlice";
+
+
 
 export default function Header() {
     const path = useLocation().pathname;
     const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
     const { theme } = useSelector((state) => state.theme);
+
+    const handleSignOut = async () => {
+      try {
+        const res = await fetch("/api/user/signout", {
+          method: "POST"
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signOutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -60,9 +80,7 @@ export default function Header() {
                 <Dropdown.Item>Profile</Dropdown.Item>
               </Link>   
               <Dropdown.Divider /> 
-              <Link to="#">
-                <Dropdown.Item>Sign out</Dropdown.Item>
-              </Link>  
+                <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
             </Dropdown>
           ) : (
             <Link to="/sign-in">
