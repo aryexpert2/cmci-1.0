1d01ec106fad0736e15ecd1d14e5c23016813974
aryexpert2 <cedouariel@gmail.com>
2024-02-16 19:58:10 -0500

    add create a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        image: {
+            type: String,
+            default: "https://churchanswers.com/wp-content/uploads/2022/03/Blog-Article-Picture-17.png",
+        },
+        category: {
+            type: String,
+            default: "uncategorized",
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+    }, { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
