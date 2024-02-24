14993cbebdb0a767bbfdc4c86d7a87871510d88a
aryexpert2 <cedouariel@gmail.com>
2024-02-23 22:11:30 -0500

    add dashboard overview to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,50 +1,45 @@
-import { useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import {
-   DashComments, 
-   DashPosts, 
-   DashProfile, 
-   DashUsers,
-   DashSidebar,
-} from '../components';
+  DashComments,
+  DashPosts,
+  DashProfile,
+  DashUsers,
+  DashSidebar,
+  DashboardComp,
+} from "../components";
 
 export default function Dashboard() {
   const location = useLocation();
-  const [tab, setTab] = useState('');
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab'); // recupère la valeur du paramètre tab passé à l'URL
+    const tabFromUrl = urlParams.get("tab"); // recupère la valeur du paramètre tab passé à l'URL
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
-
         {/** sidebar */}
         <DashSidebar />
       </div>
 
-        {/** profile */}
-      {
-        tab === 'profile' && <DashProfile />
-      }
+      {/** profile */}
+      {tab === "profile" && <DashProfile />}
 
-        {/** posts */}
-      {
-        tab === 'posts' && <DashPosts />
-      }
+      {/** posts */}
+      {tab === "posts" && <DashPosts />}
 
-        {/** users */}
-      {
-        tab === 'users' && <DashUsers />
-      }
+      {/** users */}
+      {tab === "users" && <DashUsers />}
 
-       {/** comments */}
-       {
-        tab === 'comments' && <DashComments />
-      }
+      {/** comments */}
+      {tab === "comments" && <DashComments />}
+
+      {/** dashboard Comp */}
+      {tab === "dash" && <DashboardComp />}
     </div>
-  )
+  );
 }
