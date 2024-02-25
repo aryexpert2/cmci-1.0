bfaa08a04c6596b3f01f288cb898e66671054347
aryexpert2 <cedouariel@gmail.com>
2024-02-25 16:30:24 -0500

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>About CmCi</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              CmCi is a full-stack web application that allows users to
+               create, read, update, and delete posts. It also allows users to search for posts by title, category
+               , and sort posts by date. The application is built with the MERN stack 
+               (MongoDB, Express, React, Node.js) and is deployed to Heroku. 
+            </p>
+            <p>
+              CmCi is a full-stack web application that allows users to
+               create, read, update, and delete posts. It also allows users to search for posts by title, category
+               , and sort posts by date. The application is built with the MERN stack 
+               (MongoDB, Express, React, Node.js) and is deployed to Heroku. 
+            </p>
+            <p>
+              CmCi is a full-stack web application that allows users to
+               create, read, update, and delete posts. It also allows users to search for posts by title, category
+               , and sort posts by date. The application is built with the MERN stack 
+               (MongoDB, Express, React, Node.js) and is deployed to Heroku. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { CallToAction } from '../components'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Harum facilis laborum officia est sequi exercitationem cum molestias quas itaque? 
+        Est rerum sapiente voluptas. Enim, voluptas aliquid autem officiis corporis consectetur?</p>
+        <CallToAction />
+    </div>
   )
 }
